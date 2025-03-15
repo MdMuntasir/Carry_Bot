90a8eeb308b7f0fa2949482273828644b9930972
Muntasir <md.muntasir2700@gmail.com>
2025-03-15 18:02:10 +0600

    Updated BLOC

--- carry_bot/lib/features/home/presentation/bloc/home_state.dart
+++ carry_bot/lib/features/home/presentation/bloc/home_state.dart
@@ -1,5 +1,19 @@
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+
 sealed class HomeState{
   const HomeState();
 }
 
 class HomeInitialState extends HomeState{}
+
+class HomeScanningDevices extends HomeState{}
+
+class HomeScannedDevices extends HomeState{
+  final List<ScanResult> devices;
+  const HomeScannedDevices(this.devices);
+}
+
+class HomeScanFailedState extends HomeState{
+  final String error;
+  const HomeScanFailedState(this.error);
+}

b1fdf8cedae32144d71a51e4929c1a852e2e6d8c
Muntasir <md.muntasir2700@gmail.com>
2025-03-15 18:01:51 +0600

    Updated BLOC

--- carry_bot/lib/features/home/presentation/bloc/home_bloc.dart
+++ carry_bot/lib/features/home/presentation/bloc/home_bloc.dart
@@ -1,9 +1,21 @@
+import 'dart:async';
+
 import 'package:carry_bot/features/home/presentation/bloc/home_event.dart';
 import 'package:carry_bot/features/home/presentation/bloc/home_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeBloc extends Bloc<HomeEvent,HomeState>{
   HomeBloc():super(HomeInitialState()){
-
+    on<HomeInitialEvent>(homeInitialEvent);
+    on<HomeClickedDeviceEvent>(homeClickedDeviceEvent);
+    on<HomeClickedRefreshEvent>(homeClickedRefreshEvent);
   }
+
+  FutureOr<void> homeInitialEvent(HomeInitialEvent event,Emitter<HomeState> emit,){}
+
+  FutureOr<void> homeClickedDeviceEvent(HomeClickedDeviceEvent event,Emitter<HomeState> emit,){}
+
+  FutureOr<void> homeClickedRefreshEvent(HomeClickedRefreshEvent event,Emitter<HomeState> emit,){}
+
+
 }

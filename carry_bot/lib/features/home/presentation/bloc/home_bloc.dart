a96982cdd95b0d0eaf35f0154a90c898a49e3402
Muntasir <md.muntasir2700@gmail.com>
2025-03-15 17:44:48 +0600

    Updated BLOC

--- carry_bot/lib/features/home/presentation/bloc/home_bloc.dart
+++ carry_bot/lib/features/home/presentation/bloc/home_bloc.dart
@@ -3,5 +3,7 @@ import 'package:carry_bot/features/home/presentation/bloc/home_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeBloc extends Bloc<HomeEvent,HomeState>{
-  HomeBloc():super(HomeInitialState()){}
+  HomeBloc():super(HomeInitialState()){
+
+  }
 }

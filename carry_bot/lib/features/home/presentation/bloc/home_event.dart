6c2cf369e00044d4e6c87ec5d1877bc53757efd8
Muntasir <md.muntasir2700@gmail.com>
2025-03-16 22:43:56 +0600

    Updated BLOC

--- carry_bot/lib/features/home/presentation/bloc/home_event.dart
+++ carry_bot/lib/features/home/presentation/bloc/home_event.dart
@@ -1,9 +1,21 @@
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+
 sealed class HomeEvent{
   const HomeEvent();
 }
 
-class HomeInitialEvent extends HomeEvent{}
+class HomeInitialEvent extends HomeEvent{
 
-class HomeClickedDeviceEvent extends HomeEvent{}
+}
 
-class HomeClickedRefreshEvent extends HomeEvent{}
+class HomeScannedDevicesEvent extends HomeEvent{
+  final List<ScanResult> devices;
+  const HomeScannedDevicesEvent(this.devices);
+}
+
+class HomeClickedDeviceEvent extends HomeEvent{
+  final BluetoothDevice device;
+  const HomeClickedDeviceEvent(this.device);
+}
+
+

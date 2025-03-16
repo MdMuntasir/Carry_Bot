69b0e52458cd981048ecb34b44cc54244505f805
Muntasir <md.muntasir2700@gmail.com>
2025-03-16 22:43:22 +0600

    Updated BLOC

--- carry_bot/lib/main.dart
+++ carry_bot/lib/main.dart
@@ -1,24 +1,32 @@
 import 'package:carry_bot/config/theme/Themes.dart';
-import 'package:carry_bot/features/device/presentation/pages/device_page.dart';
+import 'package:carry_bot/features/home/presentation/bloc/home_bloc.dart';
+import 'package:carry_bot/injection_Container.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'features/home/presentation/pages/homepage.dart';
 
-void main() {
+void main() async {
+  await initializeDependency();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: darkTheme,
-      home: const HomePage(),
+      home: MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (_) => serviceLocator<HomeBloc>()),
+        ],
+        child: const HomePage(),
+      ),
     );
   }
 }

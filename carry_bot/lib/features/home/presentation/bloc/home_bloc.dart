943ed16ec01b06d8ea477c4b59b6b88aa072db39
Muntasir <md.muntasir2700@gmail.com>
2025-03-16 22:44:04 +0600

    Updated BLOC

--- carry_bot/lib/features/home/presentation/bloc/home_bloc.dart
+++ carry_bot/lib/features/home/presentation/bloc/home_bloc.dart
@@ -1,21 +1,61 @@
 import 'dart:async';
 
+import 'package:carry_bot/core/common/client/client_connect.dart';
 import 'package:carry_bot/features/home/presentation/bloc/home_event.dart';
 import 'package:carry_bot/features/home/presentation/bloc/home_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
-class HomeBloc extends Bloc<HomeEvent,HomeState>{
-  HomeBloc():super(HomeInitialState()){
+class HomeBloc extends Bloc<HomeEvent, HomeState> {
+  final BLEService bleService;
+  HomeBloc({
+    required this.bleService,
+  }) : super(HomeInitialState()) {
     on<HomeInitialEvent>(homeInitialEvent);
     on<HomeClickedDeviceEvent>(homeClickedDeviceEvent);
-    on<HomeClickedRefreshEvent>(homeClickedRefreshEvent);
+    on<HomeScannedDevicesEvent>(homeScannedDevicesEvent);
   }
 
-  FutureOr<void> homeInitialEvent(HomeInitialEvent event,Emitter<HomeState> emit,){}
+  FutureOr<void> homeInitialEvent(
+    HomeInitialEvent event,
+    Emitter<HomeState> emit,
+  ) async {
+    emit(HomeScanningDevices());
 
-  FutureOr<void> homeClickedDeviceEvent(HomeClickedDeviceEvent event,Emitter<HomeState> emit,){}
+    await for (BluetoothAdapterState state in FlutterBluePlus.adapterState) {
+      if (state == BluetoothAdapterState.off) {
+        emit(HomeNoBluetoothConnection());
+      } else if (state == BluetoothAdapterState.on) {
 
-  FutureOr<void> homeClickedRefreshEvent(HomeClickedRefreshEvent event,Emitter<HomeState> emit,){}
+        await bleService.startScanning();
+
+        emit(HomeScannedDevices());
+
+      }
+    }
+  }
+
+  FutureOr<void> homeClickedDeviceEvent(
+    HomeClickedDeviceEvent event,
+    Emitter<HomeState> emit,
+  ) async {
+    if(event.device.platformName == "Carry Bot"){
+      bool connected = await bleService.connectToDevice(event.device);
+      connected
+          ? emit(HomeDeviceConnected(event.device))
+          : emit(HomeDeviceConnectionFailed("Couldn't connect to device"));
+    }
+    else{
+      emit(HomeDeviceConnectionFailed("This device is not a robot"));
+    }
+  }
 
 
+
+  FutureOr<void> homeScannedDevicesEvent(
+      HomeScannedDevicesEvent event,
+      Emitter<HomeState> emit,
+      ) async {
+    emit(HomeScannedDevices());
+  }
 }

e5f1e1b2c505574b93f1e9ede147990e4cdf5822
Muntasir <md.muntasir2700@gmail.com>
2025-03-17 14:25:39 +0600

    Device UI updated

--- carry_bot/lib/features/device/presentation/widgets/car_controller.dart
+++ carry_bot/lib/features/device/presentation/widgets/car_controller.dart
@@ -9,9 +9,72 @@ class CarController extends StatelessWidget {
     double w = MediaQuery.sizeOf(context).width;
     double h = MediaQuery.sizeOf(context).height;
     return Container(
-      height: h*.3,
+      height: h * .5,
       width: w,
-      child: Center(child: CarButton(),),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+        color: Color(0xFF183D3D),
+      ),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Positioned(
+            top: -30,
+            child: CarButton(
+              rotate: 0,
+            ),
+          ),
+          Positioned(
+            right: -15,
+            top: -15,
+            child: CarButton(
+              rotate: 45,
+            ),
+          ),
+          Positioned(
+            right: -15,
+            child: CarButton(
+              rotate: 90,
+            ),
+          ),
+          Positioned(
+            right: -15,
+            bottom: -15,
+            child: CarButton(
+              rotate: 135,
+            ),
+          ),
+          Positioned(
+            bottom: -30,
+            child: CarButton(
+              rotate: 180,
+            ),
+          ),
+          Positioned(
+            left: -15,
+            bottom: -15,
+            child: CarButton(
+              rotate: 225,
+            ),
+          ),
+          Positioned(
+            left: -15,
+            child: CarButton(
+              rotate: 270,
+            ),
+          ),
+          Positioned(
+            left: -15,
+            top: -15,
+            child: CarButton(
+              rotate: 315,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }

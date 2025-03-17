b5061741548f47c87feb8f01c192f31f39ca8460
Muntasir <md.muntasir2700@gmail.com>
2025-03-17 17:31:42 +0600

    Device UI updated

--- carry_bot/lib/features/device/presentation/widgets/car_button.dart
+++ carry_bot/lib/features/device/presentation/widgets/car_button.dart
@@ -1,31 +1,70 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import '../../../../core/common/client/client_connect.dart';
+import '../../../../injection_Container.dart';
 
-class CarButton extends StatelessWidget {
+class CarButton extends StatefulWidget {
   final double rotate;
+  final String move;
   const CarButton({
     super.key,
     this.rotate = 0,
+    required this.move,
   });
+
+  @override
+  State<CarButton> createState() => _CarButtonState();
+}
+
+class _CarButtonState extends State<CarButton> {
+  bool tapped = false, sentData = false;
+  Timer? timer;
 
   @override
   Widget build(BuildContext context) {
     double h = MediaQuery.sizeOf(context).height;
-    double angle = (3.1416 * rotate) / 180;
+    double angle = (3.1416 * widget.rotate) / 180;
 
     return Transform.rotate(
       angle: angle,
-      child: ClipPath(
-        clipper: CustomClipPath(),
-        child: Container(
-          height: h * .3,
-          width: h * .25,
-          decoration: BoxDecoration(
-            color: Color(0xFF93B1A6),
-            shape: BoxShape.values[0],
-          ),
-          child: Icon(
-            Icons.keyboard_double_arrow_up,
-            color: Color(0xFF183D3D),
+      child: InkWell(
+        onTapDown: (_) {
+          setState(() {
+            tapped = true;
+          });
+          timer =
+              Timer.periodic(const Duration(milliseconds: 100), (timer) async {
+            sentData = await serviceLocator<BLEService>().sendData(widget.move);
+          });
+          if (!sentData) {
+            timer?.cancel();
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                duration: Duration(milliseconds: 1500),
+                content: Text("Unfortunately buttons not working"),
+              ),
+            );
+          }
+        },
+        onTapUp: (_) {
+          setState(() {
+            tapped = false;
+          });
+          timer?.cancel();
+        },
+        child: ClipPath(
+          clipper: CustomClipPath(),
+          child: Container(
+            height: h * .08,
+            width: h * .05,
+            decoration: BoxDecoration(
+              color: tapped ? Colors.white : Color(0xFF93B1A6),
+              shape: BoxShape.values[0],
+            ),
+            child: Icon(
+              Icons.keyboard_double_arrow_up,
+              color: Color(0xFF183D3D),
+            ),
           ),
         ),
       ),
@@ -40,13 +79,13 @@ class CustomClipPath extends CustomClipper<Path> {
     double h = size.height;
 
     Path path_0 = Path();
-    path_0.moveTo(w * 0.4166667, h * 0.6000000);
-    path_0.lineTo(w * 0.5833333, h * 0.6000000);
-    path_0.lineTo(w * 0.5833333, h * 0.4600000);
-    path_0.quadraticBezierTo(
-        w * 0.5008333, h * 0.3115000, w * 0.5000000, h * 0.3500000);
-    path_0.quadraticBezierTo(
-        w * 0.5008333, h * 0.3115000, w * 0.4166667, h * 0.4600000);
+    path_0.moveTo(0, h * 0.5028571);
+    path_0.lineTo(0, h);
+    path_0.lineTo(w, h);
+    path_0.lineTo(w, h * 0.4985714);
+    path_0.quadraticBezierTo(w * 0.4811667, h * -0.0788429, w * 0.4991667, 0);
+    path_0.quadraticBezierTo(w * 0.5087583, h * -0.0664143, 0, h * 0.5028571);
+    path_0.close();
 
     return path_0;
   }

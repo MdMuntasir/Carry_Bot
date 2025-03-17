8800b48dc5e71c92fa319fa62ab1b1a88b4c24f8
Muntasir <md.muntasir2700@gmail.com>
2025-03-17 17:31:13 +0600

    Device functionality updated

--- carry_bot/lib/features/device/data/repository/device_repo_implement.dart
+++ carry_bot/lib/features/device/data/repository/device_repo_implement.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:carry_bot/core/connection%20state/data_state.dart';
 import 'package:carry_bot/core/network/connection_checker.dart';
+import 'package:carry_bot/features/device/data/data%20source/ble_source.dart';
 import 'package:mqtt_client/mqtt_server_client.dart';
 
 import '../../domain/repository/device_repository.dart';
@@ -10,14 +11,14 @@ import '../data source/remote_source.dart';
 class DeviceRepositoryImplementation implements DeviceRepository {
   final MqttServerClient client;
   final RemoteDeviceData remoteDeviceData;
+  final BleDeviceData bleDeviceData;
   final ConnectionChecker connectionChecker;
-  Function(String)? onMessageReceived;
 
   DeviceRepositoryImplementation(
     this.client,
     this.remoteDeviceData,
+    this.bleDeviceData,
     this.connectionChecker,
-    this.onMessageReceived,
   );
 
   @override
@@ -37,9 +38,14 @@ class DeviceRepositoryImplementation implements DeviceRepository {
   Future<DataState<String>> subscribe(
     MqttServerClient client,
     String topic,
+    Function(String)? onMessageReceived,
   ) async {
     if (await connectionChecker.isConnected) {
-      remoteDeviceData.mqttSubscribeData(client, topic, onMessageReceived,);
+      remoteDeviceData.mqttSubscribeData(
+        client,
+        topic,
+        onMessageReceived,
+      );
       return DataSuccess("Subscribed");
     }
     return DataFailed("No Internet Connection");
@@ -63,5 +69,17 @@ class DeviceRepositoryImplementation implements DeviceRepository {
       }
     }
     return DataFailed("No Internet Connection");
+  }
+
+  @override
+  Future<void> listenBle(
+    Function(String)? onMessageReceived,
+  ) async{
+    await bleDeviceData.listenMessage(onMessageReceived);
+  }
+
+  @override
+  Future<DataState> sendMessageBle(String message,) async{
+    return await bleDeviceData.sendMessage(message);
   }
 }

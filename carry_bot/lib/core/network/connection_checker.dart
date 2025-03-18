3f2c07d2f981b46fc5ad29b89b20459b92c29ae9
Muntasir <md.muntasir2700@gmail.com>
2025-03-18 08:36:17 +0600

    Device functionality updated

--- carry_bot/lib/core/network/connection_checker.dart
+++ carry_bot/lib/core/network/connection_checker.dart
@@ -1,13 +1,18 @@
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 
 abstract interface class ConnectionChecker {
   Future<bool> get isConnected;
+  Future<bool> get isBleConnected;
 }
 
 class ConnectionCheckerImpl implements ConnectionChecker {
   final InternetConnection internetConnection;
-
   ConnectionCheckerImpl(this.internetConnection);
 
+  @override
   Future<bool> get isConnected async => internetConnection.hasInternetAccess;
+
+  @override
+  Future<bool> get isBleConnected async => FlutterBluePlus.adapterStateNow == BluetoothAdapterState.on;
 }

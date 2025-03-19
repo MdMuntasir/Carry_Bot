e49e1cf4771f544876ff27a0aa1416fc700c330e
Muntasir <md.muntasir2700@gmail.com>
2025-03-19 16:55:08 +0600

    Device functionality updated

--- carry_bot/lib/features/device/presentation/bloc/device_state.dart
+++ carry_bot/lib/features/device/presentation/bloc/device_state.dart
@@ -3,3 +3,13 @@ sealed class DeviceState{
 }
 
 class DeviceInitialState extends DeviceState{}
+
+class DeviceLoadingState extends DeviceState{}
+
+class DeviceListeningState extends DeviceState{}
+
+class DeviceFailedState extends DeviceState{
+  final String error;
+  const DeviceFailedState(this.error);
+}
+

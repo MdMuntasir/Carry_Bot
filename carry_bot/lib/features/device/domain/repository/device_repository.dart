043f85985025c7942396131ce97e48afdf95de00
Muntasir <md.muntasir2700@gmail.com>
2025-03-19 16:55:00 +0600

    Device functionality updated

--- carry_bot/lib/features/device/domain/repository/device_repository.dart
+++ carry_bot/lib/features/device/domain/repository/device_repository.dart
@@ -1,9 +1,10 @@
 import 'package:carry_bot/core/connection%20state/data_state.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:mqtt_client/mqtt_server_client.dart';
 
 abstract class DeviceRepository {
   Future<void> listenBle(
-    Function(String)? onMessageReceived,
+    BuildContext context,
   );
 
   Future<DataState> sendMessageBle(

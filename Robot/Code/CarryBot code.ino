f3bdc26ab2d463560199aaefdfb15429a7144b3d
Md. Muntasir Hossain <64591064+MdMuntasir@users.noreply.github.com>
2025-03-19 17:28:00 +0600

    Update CarryBot code.ino

--- Robot/Code/CarryBot code.ino
+++ Robot/Code/CarryBot code.ino
@@ -1,26 +1,15 @@
-#include <WiFi.h>
-#include <WiFiClientSecure.h>
-#include <PubSubClient.h>
 #include <ArduinoJson.h>
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
 
-#define TRIG_PIN 5  
-#define ECHO_PIN 18 
+
+#define distanceTrig 5
+#define distanceEcho 18
+#define depthTrig 19
+#define depthEcho 21
 
 
-const char* ssid = "Free net";
-const char* password = "koitamna";
-const char* mqtt_server = "ca4608428ecc49bf8d21c278b450ef13.s1.eu.hivemq.cloud";
-const int mqtt_port = 8883; 
-const char* mqtt_user = "Muntasir";
-const char* mqtt_pass = "CarryB0T";
-const char* mqtt_topic = "distanceSensor";
-unsigned long lastMsg = 0;
-
-
-// BLE system variables
 
 BLEServer* pServer = NULL;
 BLECharacteristic* pCharacteristic = NULL;
@@ -30,50 +19,9 @@ BLECharacteristic* pCharacteristic = NULL;
 bool deviceConnected = false;
 
 
-
-WiFiClientSecure espClient;
-PubSubClient client(espClient);
-
-
-// Root CA Certificate for HiveMQ Cloud
-const char* root_ca = \
-"-----BEGIN CERTIFICATE-----\n" \
-"MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n" \
-"TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n" \
-"cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n" \
-"WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n" \
-"ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n" \
-"MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n" \
-"h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n" \
-"0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n" \
-"A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n" \
-"T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n" \
-"B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n" \
-"B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n" \
-"KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n" \
-"OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n" \
-"jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw\n" \
-"qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n" \
-"rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n" \
-"HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq\n" \
-"hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n" \
-"ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n" \
-"3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n" \
-"NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n" \
-"ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n" \
-"TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n" \
-"jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n" \
-"oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n" \
-"4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n" \
-"mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n" \
-"emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n" \
-"-----END CERTIFICATE-----\n";
-
-
-
 class MyCallbacks : public BLECharacteristicCallbacks {
   void onWrite(BLECharacteristic *pCharacteristic) {
-    std::string value = pCharacteristic->getValue();
+    String value = pCharacteristic->getValue();
     if (value.length() > 0) {
       Serial.print("📩 Received: ");
       Serial.println(value.c_str()); // Print received data
@@ -93,90 +41,9 @@ class MyServerCallbacks: public BLEServerCallbacks {
     void onDisconnect(BLEServer* pServer) {
         deviceConnected = false;
         Serial.println("Device Disconnected!");
-        BLEDevice::startAdvertising();  // Restart advertising
+        BLEDevice::startAdvertising();  
     }
 };
-
-
-
-void setup_wifi() {
-  delay(10);
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("Wifi Status: ");
-  Serial.println(WiFi.status());
-}
-
-
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-}
-
-
-
-void reconnect() {
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    String clientId = "ESP32Client-";
-    clientId += String(random(0xffff), HEX);
-    if (client.connect(clientId.c_str(), mqtt_user, mqtt_pass)) {
-      Serial.println("connected");
-      client.subscribe("testMessage");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      delay(5000);
-    }
-  }
-}
-
-
-
-float getDistance() {
-    digitalWrite(TRIG_PIN, LOW);
-    delayMicroseconds(2);
-    digitalWrite(TRIG_PIN, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(TRIG_PIN, LOW);
-
-    long duration = pulseIn(ECHO_PIN, HIGH);
-    float distance = duration * 0.034 / 2; // Convert to cm
-    return distance;
-}
-
-
-
-
-void publishJson() {
-    StaticJsonDocument<200> jsonDoc;
-    jsonDoc["distance_cm"] = getDistance();
-
-    char buffer[256];
-    serializeJson(jsonDoc, buffer);
-
-    Serial.print("📤 Publishing: ");
-    Serial.println(buffer);
-    client.publish(mqtt_topic, buffer);
-}
 
 
 
@@ -266,6 +133,7 @@ void bleInit(){
 
 
   pCharacteristic->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
+  pCharacteristic->setCallbacks(new MyCallbacks());
   pService->start();
 
 
@@ -280,53 +148,102 @@ void bleInit(){
 }
 
 
-
-// void bleInit(){
-//   BLEDevice::init("Carry Bot");
-//   pServer = BLEDevice::createServer();
-//   pServer->setCallbacks(new MyServerCallbacks());
-//   BLEService* pService = pServer->createService(SERVICE_UUID);
-//   pCharacteristic = pService->createCharacteristic(
-//                       CHARACTERISTIC_UUID,
-//                       BLECharacteristic::PROPERTY_READ |
-//                       BLECharacteristic::PROPERTY_WRITE |
-//                       BLECharacteristic::PROPERTY_NOTIFY
-//                   );
-//   pService->start();
-//   BLEAdvertising* pAdvertising = BLEDevice::getAdvertising();
-//   pAdvertising->addServiceUUID(SERVICE_UUID);
-//   BLEDevice::startAdvertising();
-// }
-
-
 void pinInit(){
-  pinMode(TRIG_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
+  pinMode(distanceTrig, OUTPUT);
+  pinMode(distanceEcho, INPUT);
+  pinMode(depthTrig, OUTPUT);
+  pinMode(depthEcho, INPUT);
 }
+
+
+float getDistance() {
+    digitalWrite(distanceTrig, LOW);
+    delayMicroseconds(2);
+    digitalWrite(distanceTrig, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(distanceTrig, LOW);
+
+    long duration = pulseIn(distanceEcho, HIGH);
+    float distance = duration * 0.034 / 2; 
+    return distance;
+}
+
+float getDepth(){
+    digitalWrite(depthTrig, LOW);
+    delayMicroseconds(2);
+    digitalWrite(depthTrig, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(depthTrig, LOW);
+
+    long duration = pulseIn(depthEcho, HIGH);
+    float distance = duration * 0.034 / 2; // Convert to cm
+    return distance;
+}
+
+
+
+const char* getSituation(int low, int high, float warn, int val){
+    if (val < low || val > high) return "R";  // Red (Danger)
+    if ((val > low && val < low + warn) || (val < high && val > high - warn)) return "Y";  // Yellow (Warning)
+    return "G";  // Green (Safe)
+}
+
+
+
+void sensorJson(JsonObject sensor, const char* name, float value, const char* situation) {
+    sensor["name"] = name;
+    sensor["value"] = value;
+    sensor["situation"] = situation;
+}
+
+
+void sendSensorData(){
+  StaticJsonDocument<300> jsonArray;
+    JsonArray data = jsonArray.createNestedArray("sensors");
+
+    float distance = getDistance();
+
+    
+    JsonObject sensor1 = data.createNestedObject();
+    sensorJson(sensor1, "Distance Sensor",distance,getSituation(10,50,1,distance));
+
+    JsonObject sensor2 = data.createNestedObject(); // Here write code for depth sensor
+    sensorJson(sensor2, "Depth Sensor",distance,getSituation(10,30,5,distance));
+
+    JsonObject sensor3 = data.createNestedObject(); // Here write code for weight sensor
+    sensorJson(sensor3, "Weight Sensor",distance,getSituation(500,5000,100,distance));
+
+    char buffer[512];  
+    serializeJson(jsonArray, buffer);
+
+    Serial.print("📤 Sending JSON List: ");
+    Serial.println(buffer);
+
+    if (deviceConnected) {
+        pCharacteristic->setValue(buffer);
+        pCharacteristic->notify();
+    }
+}
+
+
+
+void moveCar(const char* move){
+  // Write this method where the method will recieve move as front, fr, right, br, back, bl, left, fl
+}
+
 
 
 void setup() {
   Serial.begin(115200);
   pinInit();
   bleInit();
-  
 
-  // setup_wifi();
-  // espClient.setCACert(root_ca);
-  
-
-  // client.setServer(mqtt_server, mqtt_port);
-  // client.setCallback(callback);
 }
-
 
 void loop() {
 
-  if (deviceConnected) {
-        pCharacteristic->setValue("Distance: 25.3 cm");
-        pCharacteristic->notify();  // Notify Android
-        Serial.println("📤 Data sent: Distance: 25.3 cm");
-        delay(1000);
-    }
-}
+    sendSensorData();
 
+    delay(500);
+
+}

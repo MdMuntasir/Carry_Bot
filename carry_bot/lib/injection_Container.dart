4d6fbe79bf60fe7d6423eabb02c06cbc64181b6b
Muntasir <md.muntasir2700@gmail.com>
2025-03-19 16:55:14 +0600

    Device functionality updated

--- carry_bot/lib/injection_Container.dart
+++ carry_bot/lib/injection_Container.dart
@@ -1,4 +1,5 @@
 import 'package:carry_bot/core/common/client/client_connect.dart';
+import 'package:carry_bot/features/device/data/data%20source/sensor_data.dart';
 import 'package:carry_bot/features/home/presentation/bloc/home_bloc.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
@@ -9,6 +10,8 @@ final serviceLocator = GetIt.instance;
 Future<void> initializeDependency() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  serviceLocator.registerLazySingleton<SensorInformation>(()=> SensorInformation());
 
   serviceLocator.registerLazySingleton<BLEService>(
     () => BLEService(),

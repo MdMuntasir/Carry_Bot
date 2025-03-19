acf9f5b7f5949a88e92ed309a6132506709e1709
Muntasir <md.muntasir2700@gmail.com>
2025-03-19 16:51:07 +0600

    Device BLOC updated

--- carry_bot/lib/features/device/presentation/bloc/device_bloc.dart
+++ carry_bot/lib/features/device/presentation/bloc/device_bloc.dart
@@ -1,10 +1,19 @@
+import 'dart:async';
+import 'package:carry_bot/features/device/domain/repository/device_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'device_event.dart';
 import 'device_state.dart';
 
 class DeviceBloc extends Bloc<DeviceEvent,DeviceState>{
-  DeviceBloc():super(DeviceInitialState()){
+  final DeviceRepository deviceRepository;
 
+  DeviceBloc({required this.deviceRepository,}):super(DeviceInitialState()){
+    on<DeviceInitialEvent>(deviceInitialEvent);
+  }
+
+  FutureOr<void> deviceInitialEvent(DeviceInitialEvent event, Emitter<DeviceState> emit,) async{
+    emit(DeviceLoadingState());
+    await deviceRepository.listenBle(event.context);
+    emit(DeviceListeningState());
   }
 }

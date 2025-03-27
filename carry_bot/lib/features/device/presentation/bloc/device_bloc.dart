95083b3e85431f43044fbaeb0fcf49aab8bc4399
Muntasir <md.muntasir2700@gmail.com>
2025-03-27 20:24:25 +0600

    Device Bloc updated

--- carry_bot/lib/features/device/presentation/bloc/device_bloc.dart
+++ carry_bot/lib/features/device/presentation/bloc/device_bloc.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'package:carry_bot/core/common/client/client_connect.dart';
 import 'package:carry_bot/features/device/domain/usecase/listen_ble_usecase.dart';
 import 'package:carry_bot/features/device/domain/usecase/sendMessage_ble_usecase.dart';
+import 'package:carry_bot/injection_Container.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'device_event.dart';
 import 'device_state.dart';
@@ -12,8 +14,11 @@ class DeviceBloc extends Bloc<DeviceEvent, DeviceState> {
   DeviceBloc({
     required this.listenBleUseCase,
     required this.sendBleMessageUseCase,
-  }) : super(DeviceInitialState()) {
+  }) : super(DeviceLoadingState()) {
     on<DeviceInitialEvent>(deviceInitialEvent);
+    on<DeviceSendMessageEvent>(deviceSendMessageEvent);
+    on<DeviceModeSwitchEvent>(deviceModeSwitchEvent);
+    on<DeviceDisconnectEvent>(deviceDisconnectEvent);
   }
 
   FutureOr<void> deviceInitialEvent(
@@ -22,8 +27,27 @@ class DeviceBloc extends Bloc<DeviceEvent, DeviceState> {
   ) async {
     emit(DeviceLoadingState());
     await listenBleUseCase.call(para: event.context);
-    emit(DeviceListeningState());
+    emit(DeviceInitialState());
   }
 
+  FutureOr<void> deviceSendMessageEvent(
+    DeviceSendMessageEvent event,
+    Emitter<DeviceState> emit,
+  ) async {
+    await sendBleMessageUseCase.call(para: event.message);
+  }
 
+  FutureOr<void> deviceModeSwitchEvent(
+    DeviceModeSwitchEvent event,
+    Emitter<DeviceState> emit,
+  ) async {
+    event.isManual ? emit(DeviceManualMode()) : emit(DeviceAutoMode());
+  }
+
+  FutureOr<void> deviceDisconnectEvent(
+      DeviceDisconnectEvent event,
+      Emitter<DeviceState> emit,
+      ) async {
+    await serviceLocator<BLEService>().disconnectDevice();
+  }
 }

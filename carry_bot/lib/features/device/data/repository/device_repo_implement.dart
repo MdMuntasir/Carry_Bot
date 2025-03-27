fdeafdd0dd208b41cd68da70c5964d876e8a4cd7
Muntasir <md.muntasir2700@gmail.com>
2025-03-27 14:22:49 +0600

    Device Bloc updated

--- carry_bot/lib/features/device/data/repository/device_repo_implement.dart
+++ carry_bot/lib/features/device/data/repository/device_repo_implement.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:carry_bot/core/common/widgets/floating_widget.dart';
@@ -16,85 +17,92 @@ import '../../domain/repository/device_repository.dart';
 import '../data source/remote_source.dart';
 
 class DeviceRepositoryImplementation implements DeviceRepository {
-  final MqttServerClient client;
-  final RemoteDeviceData remoteDeviceData;
+  // final MqttServerClient client;
+  // final RemoteDeviceData remoteDeviceData;
   final BleDeviceData bleDeviceData;
   final ConnectionChecker connectionChecker;
 
   DeviceRepositoryImplementation(
-    this.client,
-    this.remoteDeviceData,
+    // this.client,
+    // this.remoteDeviceData,
     this.bleDeviceData,
     this.connectionChecker,
   );
 
-  @override
-  Future<DataState> publish(
-    MqttServerClient client,
-    String topic,
-    String message,
-  ) async {
-    if (await connectionChecker.isConnected) {
-      remoteDeviceData.mqttPublishData(client, topic, message);
-      return DataSuccess("Published");
-    }
-    return DataFailed("No Internet Connection");
-  }
+  // @override
+  // Future<DataState> publish(
+  //   MqttServerClient client,
+  //   String topic,
+  //   String message,
+  // ) async {
+  //   if (await connectionChecker.isConnected) {
+  //     remoteDeviceData.mqttPublishData(client, topic, message);
+  //     return DataSuccess("Published");
+  //   }
+  //   return DataFailed("No Internet Connection");
+  // }
+
+  // @override
+  // Future<DataState<String>> subscribe(
+  //   MqttServerClient client,
+  //   String topic,
+  //   Function(String)? onMessageReceived,
+  // ) async {
+  //   if (await connectionChecker.isConnected) {
+  //     remoteDeviceData.mqttSubscribeData(
+  //       client,
+  //       topic,
+  //       onMessageReceived,
+  //     );
+  //     return DataSuccess("Subscribed");
+  //   }
+  //   return DataFailed("No Internet Connection");
+  // }
+
+  // @override
+  // Future<DataState> connectClient(
+  //   String userName,
+  //   String password,
+  //   String clusterURL,
+  // ) async {
+  //   if (await connectionChecker.isConnected) {
+  //     client.secure = true;
+  //     client.securityContext = SecurityContext.defaultContext;
+  //     client.keepAlivePeriod = 20;
+  //
+  //     try {
+  //       await client.connect(userName, password);
+  //     } on Exception catch (e) {
+  //       DataFailed(e.toString());
+  //     }
+  //   }
+  //   return DataFailed("No Internet Connection");
+  // }
 
   @override
-  Future<DataState<String>> subscribe(
-    MqttServerClient client,
-    String topic,
-    Function(String)? onMessageReceived,
-  ) async {
-    if (await connectionChecker.isConnected) {
-      remoteDeviceData.mqttSubscribeData(
-        client,
-        topic,
-        onMessageReceived,
-      );
-      return DataSuccess("Subscribed");
-    }
-    return DataFailed("No Internet Connection");
-  }
-
-  @override
-  Future<DataState> connectClient(
-    String userName,
-    String password,
-    String clusterURL,
-  ) async {
-    if (await connectionChecker.isConnected) {
-      client.secure = true;
-      client.securityContext = SecurityContext.defaultContext;
-      client.keepAlivePeriod = 20;
-
-      try {
-        await client.connect(userName, password);
-      } on Exception catch (e) {
-        DataFailed(e.toString());
-      }
-    }
-    return DataFailed("No Internet Connection");
-  }
-
-  @override
-  Future<void> listenBle(
+  Future<DataState> listenBle(
     BuildContext context,
   ) async {
-    await bleDeviceData.listenMessage((message) {
-      Map<String, dynamic> jsonData = jsonDecode(message);
+    try {
+      await bleDeviceData.listenMessage((message) {
+        Map<String, dynamic> jsonData = jsonDecode(message);
 
-      if (jsonData.containsKey("sensors")) {
-        List<SensorModel> sensors = [];
-        for (Map<String, dynamic> sensor in jsonData["sensors"]) {
-          sensors.add(SensorModel.fromJson(sensor));
+        if (jsonData.containsKey("sensors")) {
+          List<SensorModel> sensors = [];
+          for (Map<String, dynamic> sensor in jsonData["sensors"]) {
+            sensors.add(SensorModel.fromJson(sensor));
+          }
+          serviceLocator<SensorInformation>().setInfo(sensors);
+        } else if (jsonData.containsKey("message")) {
+          FloatingWidgetManager.showFloatingWidget(
+              context, jsonData["message"]);
         }
-        serviceLocator<SensorInformation>().setInfo(sensors);
-      } else if (jsonData.containsKey("message")) {
-        FloatingWidgetManager.showFloatingWidget(context, jsonData["message"]);
-      }
-    });
+      });
+      return DataSuccess("Successfully Listening");
+    }
+    on Exception catch(e){
+      return DataFailed(e.toString());
+    }
   }
 
   @override

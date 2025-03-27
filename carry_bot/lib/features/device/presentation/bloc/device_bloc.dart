e9013c1e260b8a10da3523dd928c33a27cef9e9e
Muntasir <md.muntasir2700@gmail.com>
2025-03-27 14:22:16 +0600

    Device Bloc updated

--- carry_bot/lib/features/device/presentation/bloc/device_bloc.dart
+++ carry_bot/lib/features/device/presentation/bloc/device_bloc.dart
@@ -1,19 +1,29 @@
 import 'dart:async';
-import 'package:carry_bot/features/device/domain/repository/device_repository.dart';
+import 'package:carry_bot/features/device/domain/usecase/listen_ble_usecase.dart';
+import 'package:carry_bot/features/device/domain/usecase/sendMessage_ble_usecase.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'device_event.dart';
 import 'device_state.dart';
 
-class DeviceBloc extends Bloc<DeviceEvent,DeviceState>{
-  final DeviceRepository deviceRepository;
+class DeviceBloc extends Bloc<DeviceEvent, DeviceState> {
+  final ListenBleUseCase listenBleUseCase;
+  final SendBleMessageUseCase sendBleMessageUseCase;
 
-  DeviceBloc({required this.deviceRepository,}):super(DeviceInitialState()){
+  DeviceBloc({
+    required this.listenBleUseCase,
+    required this.sendBleMessageUseCase,
+  }) : super(DeviceInitialState()) {
     on<DeviceInitialEvent>(deviceInitialEvent);
   }
 
-  FutureOr<void> deviceInitialEvent(DeviceInitialEvent event, Emitter<DeviceState> emit,) async{
+  FutureOr<void> deviceInitialEvent(
+    DeviceInitialEvent event,
+    Emitter<DeviceState> emit,
+  ) async {
     emit(DeviceLoadingState());
-    await deviceRepository.listenBle(event.context);
+    await listenBleUseCase.call(para: event.context);
     emit(DeviceListeningState());
   }
+
+
 }

b487e984528043d6100d430e8f3aa7387c31d39a
Muntasir <md.muntasir2700@gmail.com>
2025-03-29 06:17:17 +0600

    Features updated

--- carry_bot/lib/main.dart
+++ carry_bot/lib/main.dart
@@ -1,35 +1,43 @@
-import 'package:carry_bot/config/theme/Themes.dart';
-import 'package:carry_bot/features/device/presentation/pages/device_page.dart';
-import 'package:carry_bot/features/home/presentation/bloc/home_bloc.dart';
-import 'package:carry_bot/injection_Container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
+import 'config/theme/Themes.dart';
+import 'features/device/presentation/bloc/device_bloc.dart';
+import 'features/home/presentation/bloc/home_bloc.dart';
 import 'features/home/presentation/pages/homepage.dart';
+import 'injection_Container.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await initializeDependency();
-  runApp(const MyApp());
+  runApp(const AppLoader());
+}
+
+class AppLoader extends StatelessWidget {
+  const AppLoader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => serviceLocator<HomeBloc>()),
+        BlocProvider(create: (_) => serviceLocator<DeviceBloc>()),
+      ],
+      child: const MyApp(),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    BluetoothDevice device = BluetoothDevice(remoteId: DeviceIdentifier(""));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Carry Bot',
       theme: darkTheme,
-      home: MultiBlocProvider(
-        providers: [
-          BlocProvider(create: (_) => serviceLocator<HomeBloc>()),
-        ],
-        child: DevicePage(device: device),
-      ),
+      home: const HomePage(),
     );
   }
 }
